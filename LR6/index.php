e6d8af3089429e84d33399cdbde0d0b20f391d07
snezha0111 <snezha0111@gmail.com>
2026-05-31 22:48:07 +0300

    Доработка ЛР6. Доьавление главной страницы с ссылками на задания

---
+++ LR6/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>OOP</title>
+</head>
+<body>
+    <header>
+        <img src="Logo_Polytech_rus_main.jpg" alt="Логотип">
+        <p>Лабораторная работа №6</p>
+    </header>
+    <main>
+        <div class="task-block">
+            <p class="name_task">Задание для OOP<p>
+            <p><a href="abstractClasses.php">Абстрактный класс</a></p>
+            <p><a href="inkapsulation.php">Инкапсуляция</a></p>
+            <p><a href="interfaces.php">Интерфейсы</a></p>
+            <p><a href="nasledovanie.php">Наследование</a></p>
+        </div>
+    </main>
+    <footer>Задание для самостоятельной работы "OOP"</footer>
+</body>
+</html> 
